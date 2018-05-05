715ac94b84a17cee4a9d3e8e12a5ac6abf1cc07b
Jaymond Huynh <jhuyn016@bolt.cs.ucr.edu>
2018-05-04 17:23:09 -0700

    updated access token

--- scrapper/authentication.py
+++ scrapper/authentication.py
@@ -3,11 +3,12 @@ from tweepy import OAuthHandler
 import os
 
 def authenticate():
-    consumer_key = 'jIxauKBrCGBU8TkXZHq7KW4iv'
-    consumer_secret = 'KzK5QRAhK2GizokOaauEeFiGprhtqudAxdhVPbJPzlZDDBqbtr'
-    access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
-    access_secret = os.environ.get('TWITTER_ACCESS_SECRET')
+    consumer_key = '5phTamiIP8B6rIizoUmBFjV4v'
+    consumer_secret = '4HfI4O6HHix1inuXhof8aN2au0GKBdpQhZrZjc8u5lLxuvyDy1'
+    access_token = '904501601146101762-tklO2ZF65vRShtHxNC0nhV0a7wGbtBg'
+    access_secret = 'kq2WpalEuDexL9Zm7bfWHpW9I10yFgnYUmLJvK7dtRcv7'
 
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     return auth
+	
